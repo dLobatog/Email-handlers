05064a2ced7e6734df0276289143286861290aa5
Daniel Lobato García <elobatocs@gmail.com>
2012-05-24 02:48:11 -0400

    Added support for mixed messages with merb and rd

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -24,5 +24,18 @@ class NavigationTest < ActiveSupport::IntegrationCase
     expected = "<p>MERB template handler is <strong>cool and fast</strong>!</p>"
     assert_match expected, page.body.strip
   end
+
+  test "dual template with .merb" do
+    email = Notifier.contact("you@example.com")
+    assert_equal 2, email.parts.size
+    assert_equal "multipart/alternative", email.mime_type
+    assert_equal "text/plain", email.parts[0].mime_type
+    assert_equal "Dual templates **rocks**!",
+      email.parts[0].body.encoded.strip
+    assert_equal "text/html", email.parts[1].mime_type
+    assert_equal "<p>Dual templates <strong>rocks</strong>!</p>",
+      email.parts[1].body.encoded.strip
+  end
+  
   
 end
